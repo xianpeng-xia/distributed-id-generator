fdd859a532dfef6380936929672361bac7214a3b
xiapeng <xiaxianpeng@autohome.com.cn>
2022-06-02 00:01:30 +0800

    init

---
+++ src/main/java/com/example/generator/DistributedIdGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedIdGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedIdGeneratorApplication.class, args);
+    }
+
+}
